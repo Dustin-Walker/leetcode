3db460eb9399cc272c72f1c7a539b075eb6687e5
Dustin-Walker <dustinwalker01@gmail.com>
2025-04-07 21:04:37 -0700

    KidsWithCandies passing

--- TS/Kids with the Greatest Number of Candies/KidsWithCandies.test.ts
+++ TS/Kids with the Greatest Number of Candies/KidsWithCandies.test.ts
@@ -1,7 +1,25 @@
 import kidsWithCandies from "./KidsWithCandies";
 
-expect(kidsWithCandies([2, 3, 5, 1, 3])).toEqual(3);
-
-expect(kidsWithCandies([4, 2, 1, 1, 2])).toEqual(1);
-
-expect(kidsWithCandies([12, 1, 12])).toEqual(10);
+describe("KidsWithCandies", () => {
+  test("case 1", () => {
+    expect(kidsWithCandies([2, 3, 5, 1, 3], 3)).toEqual([
+      true,
+      true,
+      true,
+      false,
+      true,
+    ]);
+  });
+  test("case 2", () => {
+    expect(kidsWithCandies([4, 2, 1, 1, 2], 1)).toEqual([
+      true,
+      false,
+      false,
+      false,
+      false,
+    ]);
+  });
+  test("case 3", () => {
+    expect(kidsWithCandies([12, 1, 12], 10)).toEqual([true, false, true]);
+  });
+});
--- TS/Kids with the Greatest Number of Candies/KidsWithCandies.ts
+++ TS/Kids with the Greatest Number of Candies/KidsWithCandies.ts
@@ -1,8 +1,22 @@
+/*
+Leetcode question 1431
+https://leetcode.com/problems/kids-with-the-greatest-number-of-candies
+ */
+
+/**
+ *
+ * @param candies
+ * @param extraCandies
+ * @output boolean[]
+ */
 const kidsWithCandies = (
   candies: number[],
-  extraCandies?: number,
+  extraCandies: number,
 ): boolean[] => {
-  return [];
+  const highestCandyCount = Math.max(...candies);
+  return candies.map(
+    (candyCount: number) => candyCount + extraCandies >= highestCandyCount,
+  );
 };
 
 export default kidsWithCandies;
