2a2dd3eb801f2a1d306b6522ed711ba9dd83414c
Dustin-Walker <dustinwalker01@gmail.com>
2025-04-07 21:35:49 -0700

    Kids With Candies in Go

---
+++ Go/KidsWithCandies/kidsWithCandies.go
@@ -0,0 +1,25 @@
+func max(arr []int) int {
+	if len(arr) == 0 {
+		return 0
+	}
+
+	max := arr[0]
+	for _, num := range arr {
+		if num > max {
+			max = num
+		}
+	}
+	return max
+}
+
+func kidsWithCandies(candies []int, extraCandies int) []bool {
+	highestCandyCount := max(candies)
+	// If the candies[i] + extraCandies > highestCandyCount,
+	// they are higher than the highest candy count
+	candiesSize := len(candies)
+	higherThanHighestCandyCount := make([]bool, candiesSize)
+	for i := 0; i < candiesSize; i++ {
+		higherThanHighestCandyCount[i] = candies[i]+extraCandies >= highestCandyCount
+	}
+	return higherThanHighestCandyCount
+}
---
+++ Go/go.mod
@@ -0,0 +1,3 @@
+module kidsWithCandies
+
+go 1.24.2
